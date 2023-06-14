59dc6fdca96fd2d4c1145d410ba57569d60e9afa
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-06-14 20:39:23 +0530

    Changes folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { TaskForm } from './TaskForm';
-import { Task } from './Task';
+import { TaskForm } from './Components/TaskForm';
+import { Task } from './Components/Task';
 
 function App() {
   const [tasks, setTasks] = useState([]);
