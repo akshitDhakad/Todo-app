62fb2da9947642adaa78d341d1c0c648ae61467a
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-08-09 11:15:24 +0530

    Initial commit for To-Do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { TaskForm } from './Components/TaskForm';
-import { Task } from './Components/Task';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TaskForm } from "./Components/TaskForm";
+import { Task } from "./Components/Task";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -15,11 +15,11 @@ function App() {
   useEffect(() => {
     console.log(tasks);
     if (tasks.length === 0) return;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
   useEffect(() => {
-    const intialTask = JSON.parse(localStorage.getItem('tasks'));
+    const intialTask = JSON.parse(localStorage.getItem("tasks"));
     setTasks(intialTask || []);
   }, []);
 
@@ -52,48 +52,117 @@ function App() {
     });
   };
 
+  // Tiem and Date
+  const currentDate = new Date();
+
+  // Get the current date
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+
+  // Format the date as a string
+  const todaysDate = `${day}/${month}/${year}`;
+
+  // Get the current time
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+
+  // Format hours for 12-hour clock
+  const formattedHours = hours % 12 || 12;
+
+  // Determine AM or PM using ternary operator
+  const period = hours >= 12 ? "PM" : "AM";
+
+  // Format the time string
+  const currentTime = `${formattedHours}:${
+    minutes < 10 ? "0" + minutes : minutes
+  } ${period}`;
+
   return (
-    <div className="main-container">
-      <main>
-        <h1>ToDo App</h1>
-        <h2> Status : {`${numberCompleted} / ${totalTasks}`} </h2>
-        <div className="form-container">
-          <TaskForm addTasks={addTasks} />
-          <div className="task-container">
-            {tasks.map((task, index) => {
-              return (
-                <Task
-                  {...task}
-                  key={index}
-                  onRename={(newName) => {
-                    renameTask(index, newName);
-                  }}
-                  onTrash={() => {
-                    removeTask(index);
-                  }}
-                  onToggle={(newDone) => {
-                    updateDone(index, newDone);
-                  }}
-                />
-              );
-            })}
-          </div>
-          <button
-            className={`${tasks?.length === 0 ? 'hide' : 'clear-all'}`}
-            onClick={removeAllTasks}
-          >
+    <>
+      <nav className="w-full bg-[#508C9B]">
+        <h1 className="lg:text-2xl">Task Management App</h1>
+        <div className="flex items-center gap-2">
+          <div>
             <svg
-              className="remove-button"
               xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-10"
             >
-              <path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
             </svg>
-          </button>
+          </div>
+          <div className="text-xs lg:text-sm">
+            <p>Date : {todaysDate} </p>
+            <p>Time : {currentTime} </p>
+          </div>
+        </div>
+      </nav>
+      <main>
+        <div className=" bg-white lg:w-[60%] m-auto py-2 px-4">
+          <h2 className="mb-2 lg:text-xl font-semibold text-red-500">
+            {" "}
+            <span className="">
+              Completed Task :
+            </span> {`${numberCompleted}`} / {`${totalTasks}`}
+          </h2>
+          <div className="form-container">
+            <TaskForm addTasks={addTasks} />
+            <div className="task-container">
+              {tasks.map((task, index) => {
+                return (
+                  <Task
+                    {...task}
+                    key={index}
+                    onRename={(newName) => {
+                      renameTask(index, newName);
+                    }}
+                    onTrash={() => {
+                      removeTask(index);
+                    }}
+                    onToggle={(newDone) => {
+                      updateDone(index, newDone);
+                    }}
+                  />
+                );
+              })}
+            </div>
+            <button
+              className={`py-2 bg-black flex justify-center items-center gap-x-4 ${
+                tasks?.length === 0 ? "hide" : "clear-all"
+              }`}
+              onClick={removeAllTasks}
+            >
+              <span className="text-xl">Clear All</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
         </div>
       </main>
-    </div>
+      <footer></footer>
+    </>
   );
 }
 
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -3,8 +3,8 @@ import Checkbox from './Checkbox';
 export const Task = ({ currTask, done, onToggle, onTrash, onRename }) => {
   const [editMode, setEditMode] = useState(false);
   return (
-    <div className={`task ${done ? 'done' : ''}`}>
-      <div className="remove-task">
+    <div className={`task ${done ? "done bg-red-400" : "bg-green-400"}`}>
+      <div className="remove-task  flex items-center hover:cursor-pointer">
         <Checkbox
           checked={done}
           onClick={() => {
@@ -31,6 +31,7 @@ export const Task = ({ currTask, done, onToggle, onTrash, onRename }) => {
             <input
               type="text"
               value={currTask}
+              required
               onChange={(e) => {
                 onRename(e.target.value);
               }}
@@ -39,13 +40,23 @@ export const Task = ({ currTask, done, onToggle, onTrash, onRename }) => {
         )}
       </div>
 
-      <button className="svg-button" onClick={onTrash}>
+      <button
+        className="svg-button bg-white hover:bg-black border shadow-sm"
+        onClick={onTrash}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 512 512"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6 text-red-500 hover:text-white"
         >
-          <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+          />
         </svg>
       </button>
     </div>
